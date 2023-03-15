8e63c6863883ec014eda051f4a84be83b129a737
Hasyim220 <119642434+Hasyim220@users.noreply.github.com>
2023-03-16 02:42:36 +0700

    Update manuk.php
    Signed-off-by: Hasyim220 <119642434+Hasyim220@users.noreply.github.com>

--- manuk.php
+++ manuk.php
@@ -8,8 +8,8 @@ echo "\n\n";
 echo $bold.$red."[-] By Manuk Mimin \n\n".$cln;
     echo $bold . $grey . "Pilih Menu Edot\n";
     echo "===============\n\n";
-    echo $bold . $lblue . "•> $fgreen [$white 1 $fgreen]$cln Auto Register With AdaOTP$cln\n";
-    echo $bold . $lblue . "•> $fgreen [$white 2 $fgreen]$cln Manual Register With OTP$cln\n";
+    echo $bold . $lblue . "•> $fgreen [$white 1 $fgreen]$cln Auto Reff AdaOTP$cln\n";
+    echo $bold . $lblue . "•> $fgreen [$white 2 $fgreen]$cln Manual reff OTP$cln\n";
     echo $bold . $lblue . "•> $fgreen [$white 3 $fgreen]$cln Withdraw$cln\n";
   
 echo "\n";
